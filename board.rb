ab1dda0a29f0f55cff7c1300dbc3cccea7557fea
Adam + Leah <Adam+Leah@gmail.com>
2015-08-04 11:55:58 -0400

    on_board, [] methods for board

---
+++ board.rb
@@ -0,0 +1,27 @@
+class Board
+  BOARD_LENGTH = 8
+  attr_reader :grid
+
+  def initialize
+    @grid = Array.new(BOARD_LENGTH) { Array.new(BOARD_LENGTH) }
+    #populate board
+  end
+
+  def [](pos)
+    x,y = pos
+    grid[x][y]
+  end
+
+  def []=(pos, value)
+    x,y = pos
+    grid[x][y] = value
+  end
+
+  def on_board?(pos)
+    pos.all? { |coordinate| coordinate.between?(0, BOARD_LENGTH - 1) }
+  end
+
+  def has_color_piece(pos, color)
+
+  end
+end
