21095d934596a141ac9039411fc42c0547bc3fdc
App Academy Student <appacademy@Apps-Mac-mini-88.local>
2015-08-04 11:33:04 -0400

    Create Pieces class

---
+++ pieces.rb
@@ -0,0 +1,21 @@
+require 'byebug'
+
+class Piece
+  attr_accessor :position, :board
+  attr_reader :color
+
+  def initialize(position, color, board)
+    @position = position
+    @color = color
+    @board = board
+  end
+
+  def move
+    raise MissingMethodError.new("Should have implemented subclass#move")
+  end
+  
+end
+
+
+class MissingMethodError < StandardError
+end
---
+++ stepping_pieces.rb
@@ -0,0 +1,4 @@
+require_relative 'pieces'
+require 'byebug'
+
+cl
